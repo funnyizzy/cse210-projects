c66fe4367d3569d492cf3c7d051bd1f348e00ca1
Israel <iamchoul2@gmail.com>
2025-11-05 09:21:20 -0700

    SolarSystem

---
+++ sandbox/SolarSystem/Planet.cs
@@ -0,0 +1,12 @@
+class Planet
+{
+    public string _name = "";
+    public double _diameter;
+
+    public void DisplayPlanetInformation()
+
+    {
+        Console.WriteLine($"Planet Name: {_name}, Diameter: {_diameter}");
+
+    }
+}
--- sandbox/SolarSystem/Program.cs
+++ sandbox/SolarSystem/Program.cs
@@ -2,6 +2,23 @@
 {
     static void Main()
     {
-        Console.WriteLine("Solar System");
+        Planet mercury = new Planet();
+        mercury._name = "Mercury";
+        mercury._diameter = 4879.23;
+
+        Planet venus = new Planet();
+        venus._name = "Venus";
+        venus._diameter = 12104.23;
+
+        Planet earth = new Planet();
+        earth._name = "Earth";
+        earth._diameter = 12742.01;
+        
+        SolarSystem solarSystem = new SolarSystem();
+        solarSystem._solarSystem.Add(mercury);
+        solarSystem._solarSystem.Add(venus);
+        solarSystem._solarSystem.Add(earth);
+
+        solarSystem.DisplaySolarSystem();
     }  
 }
