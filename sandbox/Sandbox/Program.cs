413b009e1058978dda65e4933d8e0ddacf012fcb
Israel <iamchoul2@gmail.com>
2025-11-19 09:44:25 -0700

    Adding mp3 files

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,7 +3,19 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Sandbox World!");
+{
+        string animationString = "\\|/-";
+        int sleepTime = 250;
+        int duration = 10; 
+        int index = 0;
+        DateTime currentTime = DateTime.Now;
+        DateTime endTime = currentTime.AddSeconds(duration);
+
+        while(DateTime.Now < endTime)
+        {
+            Console.Write(animationString[index++ % animationString.Length]);
+            Thread.Sleep(sleepTime);
+            Console.Write("\b");
+        }
     }
 }
